e4dbaffb4e5851510427c306ed410ef2e11c9166
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-26 08:55:43 +0600

    header footer done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
+import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet/>
             <Footer/>
         </div>
