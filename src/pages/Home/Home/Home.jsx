c782334c51e272de1a6b6ab1a4c65ab48e676575
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-27 01:16:02 +0600

    swiper done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
 
 
 const Home = () => {
     return (
         <div>
         <Banner/>
+        <Category/>
         </div>
     );
 };
