5a381ad233f61c792d08cb06ba6bae2765ad1eb6
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-27 16:17:21 +0600

    data chnged , show all btn not working

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,10 +2,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const MenuItem = ({item}) => {
+const MenuItem = ({ item }) => {
+    const { name, image, price, recipe } = item;
     return (
-        <div>
-            
+        <div className='flex gap-2 font-serif mr-2 space-x-3 items-center'>
+            <img style={{ borderRadius: '0 200px 200px 200px' }} className='w-[120px]' src={image} alt="" />
+            <div>
+                <h3 className='text-2xl font-bold text-[#151515]'>{name}------ </h3>
+                <p>{recipe}</p>
+            </div>
+            <p className='text-xl text-[#BB8506]'>{price}</p>
+
         </div>
     );
 };
--- src/pages/PopularMenu/PopularMenu.jsx
+++ src/pages/PopularMenu/PopularMenu.jsx
@@ -1,21 +1,45 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import MenuItem from '../../components/MenuItem/MenuItem';
+import ShowAll from '../showAll/ShowAll';
 
 const PopularMenu = () => {
     const [menu, setmenu] = useState([]);
 
+
     useEffect(() => {
         fetch('/public/data/menu.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                const allItem = data;
+                const popularItem = data.filter(data => data.category === "popular")
+                setmenu(popularItem);
+                // console.log(popularItem);
+            })
     }, []);
+
+    const showAll = () => {
+       console.log("show all");
+       
+    }
     return (
         <section>
             <SectionTitle
                 heading={"From our menu"}
                 subHeading={"check it out"}>
             </SectionTitle>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 space-y-4 mt-4 mb-4'>
+                {
+                    menu.map(item =>
+                        <MenuItem key={item._id}
+                            item={item}>
+                        </MenuItem>)
+                }
+            </div>
+
+            <button onClick={() => showAll()} className='px-5 py-3 flex justify-center mb-3 bg-yellow-400 mx-auto rounded-md'> View Full Menu</button>
 
 
         </section>
