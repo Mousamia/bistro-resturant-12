42d2eb8087acd3f60bd4f0295c8c300d16c467c2
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-28 23:29:10 +0600

    parallaex

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+
+import heroImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../PopularMenu/PopularMenu';
 
 
 const Menu = () => {
@@ -8,10 +12,16 @@ const Menu = () => {
         <div>
             {/* helmet is used to change head title dynamically */}
             {/* must need to use helmet provider in main jsx */}
-           
             <Helmet>
-                <title> Bistro Boss | Menu </title>
+                <title>Bistro | Menu </title>
             </Helmet>
+
+            <Cover heroImg={heroImg}
+            title={"Our Menu"}>
+            </Cover>
+            <PopularMenu></PopularMenu>
+            <PopularMenu></PopularMenu>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Background, Parallax } from 'react-parallax';
+
+
+const Cover = ({ heroImg, title }) => {
+    return (
+
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImageStyle = {{height: '50px', maxWidth: '75px', opacity: '.1'}}
+            bgImage={heroImg}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <div className="hero h-[700px]" style={{ backgroundImage: `url(${heroImg})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">{title} </h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+
+                    </div>
+                </div>
+
+            </div>
+        </Parallax>
+
+
+    );
+};
+
+export default Cover;
