6718a322070810f375caea5a990b6a4ec31b10d4
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-06-02 01:41:27 +0600

    food  category shown

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,20 +2,22 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const FoodCard = ({ item }) => {
-    const { name, image, price, recipe } = item;
-    console.log(name);
+const FoodCard = ({item}) => {
+
+    const {name, recipe, price, image} = item
+   
+    // console.log(item);
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
+            <figure><img src={image} alt="foodpic" /></figure>
             <div className="card-body">
-                <h2 className="card-title"> {name} </h2>
-                <p>price</p>
+                <h2 className="card-title">{name}</h2>
+                <p> {recipe} </p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to cart</button>
+                    <button className="btn btn-primary">ADD</button>
                 </div>
+                <p className="text-xl bg-slate-800 text-white absolute right-4 top-0"> {price} </p>
             </div>
-            <h2 className="text-9xl mt-10">order</h2>
         </div>
     );
 };
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,60 +1,91 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import Cover from '../../Shared/Cover/Cover';
+import React, { useState } from 'react';
+import Cover from '../../Shared/Cover/Cover'
+import orderImg from '../../../assets/shop/orderImg.jpg'
+import FoodCard from '../../../components/FoodCard/FoodCard'
+import useMenu from '../../../hooks/useMenu'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-
-import orderImg from '../../../assets/shop/orderImg.jpg'
-import useMenu from '../../../hooks/useMenu';
-import FoodCard from '../../../components/FoodCard/FoodCard';
 
 
 const Order = () => {
     const [menu] = useMenu();
+    console.log(menu);
+
+    const [tabIndex, setTabIndex] = useState(0);
+
+
     const offered = menu.filter(item => item.category === "offered");
     const dessert = menu.filter(item => item.category === "dessert");
     const pizza = menu.filter(item => item.category === "pizza");
     const salad = menu.filter(item => item.category === "salad");
     const drinks = menu.filter(item => item.category === "drinks");
     const soup = menu.filter(item => item.category === "soup");
-
     return (
         <div>
-            <Cover
-                img={orderImg}
-                title={"Order Now"}>
-            </Cover>
-
-            <Tabs>
+            {/* helmet here */}
+            <Helmet>
+                <title>Bistro | Order </title>
+            </Helmet>
+            <Cover img={orderImg}></Cover>
+            {/* REACT TAB */}
+            <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
                     <Tab>SALAD</Tab>
                     <Tab>PIZZA</Tab>
-                    <Tab>SOUPS</Tab>
-                    <Tab>DESSERTS</Tab>
+                    <Tab>SOUP</Tab>
+                    <Tab>DESSERT</Tab>
                     <Tab>DRINKS</Tab>
                 </TabList>
-
                 <TabPanel>
-                    <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-1 md:grid-cols-3">
                         {
-                            salad.map(item => {
-                                <FoodCard item={item}></FoodCard>
-                            })
+                            salad.map(item => <FoodCard
+                                key={item._id}
+                                item={item}></FoodCard>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
-                   <div>contentooooooo</div>
+                    <div className="grid grid-cols-1 md:grid-cols-3">
+                        {
+                            pizza.map(item => <FoodCard
+                                key={item._id}
+                                item={item}></FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                <div>contentooooooo</div>
+                    <div className="grid grid-cols-1 md:grid-cols-3">
+                        {
+                            soup.map(item => <FoodCard
+                                key={item._id}
+                                item={item}></FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3">
+                        {
+                            dessert.map(item => <FoodCard
+                                key={item._id}
+                                item={item}></FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3">
+                        {
+                            drinks.map(item => <FoodCard
+                                key={item._id}
+                                item={item}></FoodCard>)
+                        }
+                    </div>
                 </TabPanel>
+
             </Tabs>
 
         </div>
